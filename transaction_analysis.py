9247075b4de27811565e62dea5baf8302a98fa3e
vikwillberg <105646891+vikwillberg@users.noreply.github.com>
2023-12-15 23:38:53 -0500

    Update transaction_analysis.py

--- transaction_analysis.py
+++ transaction_analysis.py
@@ -2,11 +2,13 @@ class TransactionAnalysis:
     def __init__(self, transactions):
         self.transactions = transactions
 
+    
     def display_transactions(self):
         print("Transactions:")
         for tx in self.transactions:
             print(f"ID: {tx['id']}, Sender: {tx['sender']}, Receiver: {tx['receiver']}, Amount: {tx['amount']} BTC, Timestamp: {tx['timestamp']}")
 
+    
     def calculate_balance(self, account_number):
         balance = 0.0
         for tx in self.transactions:
